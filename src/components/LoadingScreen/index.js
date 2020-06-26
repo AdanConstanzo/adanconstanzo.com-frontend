8135e2594ce24610bc6fd4676a0467da99618e0a
Adan Constanzo <aconstanzolan@gmail.com>
2020-06-26 10:39:03 -0700

    Updated loading screen. This closes #1.

---
+++ src/components/LoadingScreen/index.js
@@ -0,0 +1,11 @@
+// Libraries
+import React from 'react';
+// Component
+const LoadingScreen = () => (
+  <div className="loadingScreen" >
+    <button></button>
+    <p>Loading....</p>
+  </div>
+);
+
+export default LoadingScreen
