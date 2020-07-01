633252f5bdbe2080ac0fc030f7327dddd22ae4bf
Adan Constanzo <aconstanzolan@gmail.com>
2020-07-01 13:31:02 -0700

    Fixed URI for graphql.

--- src/utils/apolloClient.js
+++ src/utils/apolloClient.js
@@ -3,9 +3,8 @@ import { InMemoryCache } from "apollo-cache-inmemory";
 import { HttpLink } from "apollo-link-http";
 
 const cache = new InMemoryCache();
-const link = new HttpLink({
-  uri: `${process.env.REACT_APP_BACKEND_URL}/graphql`
-});
+const uri = process.env.NODE_ENV !== "development" ? 'https://api.adanconstanzo.com/graphql' : `${process.env.REACT_APP_BACKEND_URL}/graphql`
+const link = new HttpLink({ uri });
 const client = new ApolloClient({
   cache,
   link
